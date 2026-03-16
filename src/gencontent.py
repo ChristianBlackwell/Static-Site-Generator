c48d4d4065631cb97113057005ad56b04fa6c9d2
ChristianBlackwell <christian.blackwell@outlook.com>
2026-03-16 16:37:26 -0500

    Deploy static site to GitHub Pages via docs directory

--- src/gencontent.py
+++ src/gencontent.py
@@ -10,7 +10,7 @@ def extract_title(markdown):
     
     raise Exception("Not a header.")
 
-def generate_page(from_path, template_path, dest_path):
+def generate_page(from_path, template_path, dest_path, basepath):
     print(f'Generating page from {from_path} to {dest_path} using {template_path}')
     
     markdown_file = open(from_path, "r")
@@ -26,6 +26,8 @@ def generate_page(from_path, template_path, dest_path):
     
     template = template.replace("{{ Title }}", title)
     template = template.replace("{{ Content }}", html)
+    template = template.replace('href="/', 'href="' + basepath)
+    template = template.replace('src="/', 'src="' + basepath)
 
     dest_dir_path = os.path.dirname(dest_path)
     if dest_dir_path != "":
@@ -34,13 +36,13 @@ def generate_page(from_path, template_path, dest_path):
     to_file.write(template)
     to_file.close()
 
-def generate_pages_recursive(dir_path_content, template_path, dest_dir_path):
+def generate_pages_recursive(dir_path_content, template_path, dest_dir_path, basepath):
     for file in os.listdir(dir_path_content):
         path = os.path.join(dir_path_content, file)
         if os.path.isfile(path):
             dest_path = os.path.join(dest_dir_path, file)
             dest_path = Path(dest_path).with_suffix(".html")
-            generate_page(path, template_path, dest_path)
+            generate_page(path, template_path, dest_path, basepath)
         else:
             dest_path = os.path.join(dest_dir_path, file)
-            generate_pages_recursive(path, template_path, dest_path)
+            generate_pages_recursive(path, template_path, dest_path, basepath)
